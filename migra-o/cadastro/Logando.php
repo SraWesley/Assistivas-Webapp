5eed57f8a94df152920253330afbdf096ccf0632
SraWesley <anasouzawesley@hotmail.com>
2017-08-01 16:37:22 -0300

    Add files via upload

---
+++ migra-o/cadastro/Logando.php
@@ -0,0 +1,41 @@
+<?php
+	
+	$host = "localhost";
+	$usuario = "root";
+	$pwd = "";
+	$bd = "Projeto_Soletrando";
+
+	$conexao = mysqli_connect($host, $usuario, $pwd, $bd) or die (mysqli_error());
+	mysqli_select_db($conexao, $bd);
+
+?>
+
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<title> Login </title>
+	</head>
+	<body>
+		<?php
+			session_start();
+			$login = $_POST['login'];
+			$senha = $_POST['senha'];
+
+			$sql = "SELECT * FROM usuario WHERE login = '$login' AND senha = '$senha'";
+			$res = mysqli_query($conexao, $sql);
+
+			if( ($array = mysqli_fetch_array($res)) > 0 ){
+				$_SESSION['login'] = $login;
+				$_SESSION['senha'] = $senha;
+				echo "<script> alert('Você será redirecionado a página principal.'); </script>";
+				header("refresh: 3; url=home.php");
+			} else {
+
+				echo "<script> alert('Você ainda não está cadastrado. Será redirecionado para a página de cadastro.'); </script>";
+				header("refresh: 3; url=cadastro.php");
+			}
+			
+			
+		?>
+	</body>
+</html>
---
+++ migra-o/cadastro/login.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+	<title>Login</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/simple-sidebar.css" rel="stylesheet">
+	<link href="css/estilo.css" rel="stylesheet">
+	<link rel="icon" href="/favicon.ico" type="image/x-icon"/>
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+	<div id="sidebar-wrapper">
+		<ul class="sidebar-nav">
+			<li class="sidebar-brand">
+				<a href="#">
+					Soletrando
+				</a>
+			</li>
+			<li>
+				<a href="./home.html"> HOME </a>
+			</li>
+			<li>
+				<a href="./cadastro.html"> Faça seu cadastro</a>
+			</li>
+		</ul>
+	</div>
+	
+	<div class="container">
+		<h2 style="text-align: center;">LOGIN</h2>
+		<form class="form-horizontal" role="form" method="post" action="Logando.php">
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="login">Login:</label>
+				<div class="col-sm-2">
+					<input type="login" pattern="[A-Za-z0-9]{4,8}" title = "O Login deve conter entre 4 e 8 letras ou números" class="form-control" name="login" maxlength="12" placeholder="Enter Login">
+				</div> 
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="pwd">Senha:</label>
+				<div class="col-sm-3">
+					<input type="password" id="senha" pattern=".{6,}" title = "A senha deve ter mais de seis caracteres" class="form-control" required name="senha" placeholder="Enter password">
+				</div>			
+				<div class="form-group">
+					<br>	
+					<button href="home" type="submit" class="btn btn-default">Logar</button>	
+				</div>
+			</form>
+	</div>
+</body>
+</html>
