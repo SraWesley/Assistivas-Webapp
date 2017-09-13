0e997ce5099b37ddad33b3dd6edf1df7bc8cef97
Ana Carolina Souza <anasouzawesley@hotmail.com>
2017-09-13 15:31:25 -0300

    Create upload.php

---
+++ Soletrando/upload/upload.php
@@ -0,0 +1,48 @@
+<?php
+	
+	include("../database/connect.php");
+
+
+	if(isset($_FILES['arquivo'])){
+
+		$extention = strtolower(substr($_FILES['arquivo']['name'], -4));
+		$new_name = $_FILES['arquivo']['name'];
+		$path = "upload/";
+		$dificuldade = $_POST['level'];
+		$caminho = realpath($_FILES['arquivo']['name']);
+
+		move_uploaded_file($_FILES['arquivo']['tmp_name'], $path.$new_name);
+
+		$sql = "INSERT INTO upload (palavra, dificuldade, caminho) VALUES ('$new_name', '$dificuldade', '$caminho')";
+
+		if (mysqli_query($conexao, $sql)) {
+
+			echo "Upload feito com sucesso.";
+				    
+		} else {
+
+			echo "Error: " . $sql . "<br>" . mysqli_error($conexao);
+		}
+	}
+
+	//$diretorio -> close();
+?>
+
+<html>
+	<head>
+		<h1> Upload de Arquivos </h1>
+	</head>
+	<body>
+		<form action = "upload.php" method = "POST" enctype = "multipart/form-data">
+			Arquivo:
+			<input type = "file" required name = "arquivo">
+			<br><br>
+			Dificuldade:
+			<input type = "radio" required name = "level" value = "1"> Nível 1	
+			<input type = "radio" required name = "level" value = "2"> Nível 2
+			<input type = "radio" required name = "level" value = "3"> Nível 3
+			<br><br>
+			<input type = "submit" value = "Salvar">
+		</form>
+	</body>
+</html>
